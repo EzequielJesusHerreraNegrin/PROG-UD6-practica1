24ae3ed2b04f7b742d68491f44ec88dae860da94
EzequielJesusHerreraNegrin <usr3763@salesianos-lacuesta.net>
2024-04-30 12:04:23 +0200

    feat(person): creé la clase person y le añadí los atributos 'names' y 'wallet'.

---
+++ src/com/hiperTimo/element/Person.java
@@ -0,0 +1,9 @@
+package com.hiperTimo.element;
+
+import java.util.ArrayList;
+
+public class Person {
+
+    protected static String[] names = {"Sofía","Juan","María","Alejandro","Valentina","Diego","Camila","Carlos","Gabriela","Andrés","Lucía","Daniel","Isabella","Mateo","Valeria","Santiago","Ana","Nicolás","Laura","Manuel"};
+    protected static int[] wallet = {235.34, 74.12, 42.2, 87.15, 10.5, 20.82,31.2,51.5,100,15};
+}
