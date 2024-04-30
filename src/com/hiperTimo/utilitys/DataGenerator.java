b3d741e19b6e8ebf16ded156bcd7b171e30f5be9
EzequielJesusHerreraNegrin <usr3763@salesianos-lacuesta.net>
2024-04-30 14:00:24 +0200

    feat: creación de clase generadora de estáticos.         generador de nombres de clientes.         generador de nombres de productos.         generador de cartera.         generador de id de productos.         generador de precio de producto.

---
+++ src/com/hiperTimo/utilitys/DataGenerator.java
@@ -0,0 +1,29 @@
+package com.hiperTimo.utilitys;
+
+public class DataGenerator {
+    protected static String[] productName = {"Sofía","Juan","María","Alejandro","Valentina","Diego","Camila","Carlos","Gabriela","Andrés","Lucía","Daniel","Isabella","Mateo","Valeria","Santiago","Ana","Nicolás","Laura","Manuel"};
+    protected static String[] clientName = {"Café gourmet","Smartphone Xplorer","Auriculares SoundWave","Zapatillas AirRun","Camisa Evergreen","Tableta Digital Plus","Perfume Elegance","Crema Hidratante SilkSkin","Mochila Adventure","Reloj ChronoMaster","Bolígrafo Diamond","Barra de chocolate Supreme"};
+    protected static String clientWallet;
+    protected static String productPrice;
+    protected static String productID;
+
+    public static String RandomProductName(){
+        int randomNum = (int)Math.floor(Math.random() *(12 - 0 + 1) + 0);
+        return productName[randomNum];
+    }
+    public static void RandomClientName(){
+
+    }
+    public static void RandomClientWallet(){
+
+    }
+    public static float RandomProductPrice(){
+        return (float)Math.floor(Math.random() *(1000 - 0 + 1) + 0);
+    }
+    public static int RandomProductID(){
+        int randomNum = (int)Math.floor(Math.random() *(20 - 0 + 1) + 0);
+        return randomNum;
+    }
+
+
+}
