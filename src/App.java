2354da5533687119f12e77320a53a4ddeebe6036
EzequielJesusHerreraNegrin <usr3763@salesianos-lacuesta.net>
2024-04-30 12:46:05 +0200

    feat: creación de métodos estáticos que devuelven un valor aleatorio.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import com.hiperTimo.element.Person;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(Person.randomWallet());
     }
 }
--- src/com/hiperTimo/element/Person.java
+++ src/com/hiperTimo/element/Person.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 
 public class Person {
 
-    protected static String[] names = {"Sofía","Juan","María","Alejandro","Valentina","Diego","Camila","Carlos","Gabriela","Andrés","Lucía","Daniel","Isabella","Mateo","Valeria","Santiago","Ana","Nicolás","Laura","Manuel"};
-    protected static int[] wallet = {235.34, 74.12, 42.2, 87.15, 10.5, 20.82,31.2,51.5,100,15};
+    protected final static String[] names = {"Sofía","Juan","María","Alejandro","Valentina","Diego","Camila","Carlos","Gabriela","Andrés","Lucía","Daniel","Isabella","Mateo","Valeria","Santiago","Ana","Nicolás","Laura","Manuel"};
+    protected static final  float[] wallets = {235.34f, 74.12f, 42.2f, 87.15f, 10.5f, 20.82f,31.2f,51.5f,100f,15f};
+
+
+    public static   String randomName(){
+        int randomNum = (int)Math.floor(Math.random() *(20 - 0 + 1) + 0);
+        return names[randomNum];
+    }
+
+    public static   float randomWallet(){
+        int randomNum = (int)Math.floor(Math.random() *(10 - 0 + 1) + 0);
+        return wallets[randomNum];
+    }
 }
