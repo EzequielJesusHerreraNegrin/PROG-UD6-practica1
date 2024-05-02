2ea7618f0de379267610373cba1129c2161a23bf
EzequielJesusHerreraNegrin <usr3763@salesianos-lacuesta.net>
2024-05-02 20:22:33 +0200

    fix(product & data-generador): -Product.java: Corrección de error de asignación de método a la variable "name". -DataGenerator.java: Corrección de cruce de nombres de variables y valores máximos de métodos para generar nombres.

--- src/com/hiperTimo/element/Product.java
+++ src/com/hiperTimo/element/Product.java
@@ -8,7 +8,7 @@ public class Product {
     protected float price;
 
     public Product() {
-        this.name = DataGenerator.RandomClientName();
+        this.name = DataGenerator.RandomProductName();
         this.id = DataGenerator.RandomProductID();
         this.price = DataGenerator.RandomProductPrice();
     }
@@ -33,7 +33,7 @@ public class Product {
 
     public String toString(){
         return "Nombre del articulo: "+this.name+
-                "Precio del articulo: "+this.price+
-                "numero de serie : "+this.id;
+                "\nPrecio del articulo: "+this.price+
+                "\nnumero de serie : "+this.id;
     }
 }
