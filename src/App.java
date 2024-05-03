cbf4d5566d9200047aa1244fdf2aae98489654df
EzequielJesusHerreraNegrin <usr3763@salesianos-lacuesta.net>
2024-05-03 21:12:59 +0200

    fix(cashier): -Corrección de operades lógicos para que la variable "checkoutOperativeState" sea igual a false por defecto.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,16 @@
-import com.hiperTimo.element.Client;
+import java.util.Scanner;
+
+import com.hiperTimo.element.Cashier;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Cashier cajero = new Cashier();
+        cajero.openCheckoutPoint();
+        cajero.enqueueClient();
+        cajero.serveClient();
+
+        /* Client client = new Client();
+        client.FillBaskect();
+        System.out.println(client); */
     }
 }
---
+++ src/com/hiperTimo/element/Cashier.java
@@ -0,0 +1,82 @@
+package com.hiperTimo.element;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Cashier {
+    private int checkoutPoint = 1;
+    private Boolean checkoutPointOperativeEstate = false;
+    private Queue<Client> fila = new LinkedList<Client>();
+    
+    public Cashier() {
+        this.checkoutPoint = checkoutPoint;
+        this.fila = fila;
+        this.checkoutPointOperativeEstate = checkoutPointOperativeEstate;
+    }
+
+    public int getCheckoutPoint() {
+        return checkoutPoint;
+    }
+
+    public void setCheckoutPoint(int checkoutPoint) {
+        this.checkoutPoint = checkoutPoint;
+    }
+
+    public Queue<Client> getFila() {
+        return fila;
+    }
+
+    public void setFila(Queue<Client> fila) {
+        this.fila = fila;
+    }
+
+    public void enqueueClient(){
+        if (checkoutPointOperativeEstate == true) {
+            Client client = new Client();
+            fila.add(client);   
+            System.out.println("\nUn cliente esta esperando en la fila.");
+        } else {
+            System.out.println("\nLa caja no está abierta, ningún cliente esperará a ser atendido.");
+        }
+    }
+
+    public void openCheckoutPoint(){
+        if (this.checkoutPointOperativeEstate == false) {
+            this.checkoutPointOperativeEstate = true;
+            System.out.println("\nLa caja numero"+this.checkoutPoint+" se ha abierto.");
+        }
+    }
+    
+    public void closeCheckoutPoint(){
+        if (this.checkoutPointOperativeEstate == false && fila.isEmpty() == true) {
+            this.checkoutPointOperativeEstate = false;
+            System.out.println("\nLa caja numero"+this.checkoutPoint+" se ha cerrado.");
+        } else {
+            System.out.println("\nTodavía hay clientes en la cola, atiendelos antes de cerrar.");
+        }
+    }
+
+    public void serveClient(){
+        if (fila.isEmpty() == false) {
+            System.out.println(fila.poll());
+        } else{
+            System.out.println("No hay nadie esperando en la fila.");
+        }
+    }
+
+    public void checkQueue(){
+        if (fila.isEmpty() == false) {
+            System.out.println("Hay "+fila.size()+" clietnes, esperando a ser atendidos en la caja"+this.checkoutPoint+".");
+        } else{
+            System.out.println("No hay nadie esperando en la caja "+this.checkoutPoint+".");
+        }
+    }
+
+    public void closeMarket(){
+        if (this.checkoutPointOperativeEstate == false) {
+            System.out.println("El establecimiento ha cerrado sus puertas.");
+        } else{
+            System.out.println("Antes de cerrar el local, debe cerrar la caja para no recibir mas clientes.");
+        }
+    }
+}
